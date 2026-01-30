919ea85a5b1e8951de126840011df621c3b54bef
tylersense-ui <tyler.sense@gmail.com>
2026-01-31 00:56:25 +0100

    Add more RAM options for server configuration
    Expanded available RAM options to include higher values.

--- deploy/replace-pservs-no-copy.js
+++ deploy/replace-pservs-no-copy.js
@@ -41,7 +41,7 @@ export async function main(ns) {
   }
 
   // Available RAM options (must be powers of 2)
-  const ramOptions = [8, 16, 32, 64, 128, 256, 512, 1024];
+  const ramOptions = [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576];
   
   // Get target RAM from argument
   const targetRAM = ns.args[0] ? parseInt(ns.args[0]) : null;
